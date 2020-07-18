67b51241dc60c1b472b52549c32d931cd6a98c0b
Dmitry Astapov <dastapov@gmail.com>
2020-07-18 16:27:42 +0100

    Interact: check flag

--- app/src/interact.ml
+++ app/src/interact.ml
@@ -14,13 +14,24 @@ let rec interact ~protocol ~state ~vector ~eval =
   printf "\nContinue? (Ctrl-C to quit)\n%!";
   let (_ : string) = In_channel.input_line_exn In_channel.stdin in
   let res = eval Eval.(App (App (protocol, state), vector)) in
+  let flag = eval (App (Var "car", res)) in
+  let newState = eval (App (Var "car", App (Var "cdr", res))) in
+  let vector = eval (App (Var "cdr", App (Var "cdr", res))) in
   printf !"res = %{sexp: Eval.t}\n%!" res;
-  interact ~protocol ~state ~vector ~eval
+  printf !"flag = %{Eval#hum}\n%!" flag;
+  match flag with
+  | Num 0 ->
+    (* TODO: multipledraw *)
+    printf !"newState = %{Eval#hum}\n%!" newState;
+    printf !"vector = %{Eval#hum}\n%!" vector
+  | _ ->
+    (* TODO : send *)
+    interact ~protocol ~state ~vector ~eval
 ;;
 
 let run ~filename ~protocol ~state ~vector =
   let defs = Eval.load_defs_exn ~filename in
   let p str = Eval.parse_exn (String.split ~on:' ' str) in
-  let eval = Eval.eval_custom ~verbose:true ~defs |> Staged.unstage in
+  let eval = Eval.eval_custom ~verbose:false ~defs |> Staged.unstage in
   interact ~protocol:(p protocol) ~state:(p state) ~vector:(p vector) ~eval
 ;;
