1edcb51e49f84f2b01927b07b2e9c88e74658a8d
Dmitry Astapov <dastapov@gmail.com>
2020-07-19 19:55:52 +0100

    Stationary #release

--- app/src/battle.ml
+++ app/src/battle.ml
@@ -1,6 +1,6 @@
 open Core
 
-let version = "0.99 DIR-FLIP-FIX"
+let version = "0.99 STATIONARY"
 
 let maybe_create ~server_url ~api_key player_key =
   if String.equal player_key "ATTACK" || String.equal player_key "DEFEND"
