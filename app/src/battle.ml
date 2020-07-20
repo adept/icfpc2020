b74ccd477a312a57c3ccb82f17dca9a5f9c58472
Alexandru Scvortov <code@scvalex.net>
2020-07-20 01:00:35 +0100

    Lead the shots

--- app/src/battle.ml
+++ app/src/battle.ml
@@ -1,6 +1,6 @@
 open Core
 
-let version = "0.101 POWER STEERING"
+let version = "0.102 LEAD THE SHOTS"
 
 let maybe_create ~server_url ~api_key player_key =
   if String.equal player_key "ATTACK" || String.equal player_key "DEFEND"
