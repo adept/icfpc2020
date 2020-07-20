a0164b9d22fe6a4e2023ce499f4df62ec521f32e
Alexandru Scvortov <code@scvalex.net>
2020-07-20 01:38:16 +0100

    Don't crash on multiple ships

--- app/src/battle.ml
+++ app/src/battle.ml
@@ -1,6 +1,6 @@
 open Core
 
-let version = "0.103 FALL A BIT"
+let version = "0.104 DONT CRASH"
 
 let maybe_create ~server_url ~api_key player_key =
   if String.equal player_key "ATTACK" || String.equal player_key "DEFEND"
